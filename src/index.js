d3f33aa0796d438ec938280ecc5aa2494cbe6bc7
Deborah964 <fiankobeadeborah@gmail.com>
2024-11-27 15:14:35 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+let name=("Deborah");
+console.log(name);
+let age=(21);
+console.log(age);
+let x=4+4;
+console.log(x);
+let u=15 - 5;
+console.log=(u);
+let b=18/20;
+console.log(b);
+let y=4*3;
+console.log(y);
+let q=15%4;
+console.log(q);
+let light=true;
+console.log(light);
+let f=5;
+console.log(typeof(f));
+let v=("favorite");
+console.log(typeof(v));
+
+
+
